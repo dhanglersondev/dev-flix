7ebf59d4ecc3cd878c24438ab29e41deb6991dca
dhanglersondev <dhanglerson.dev@outlook.com>
2026-05-22 09:09:09 -0300

    Estrutura de pastas

---
+++ src/Pages/Filme/index.js
@@ -0,0 +1,10 @@
+function Filme() {
+  return (
+    <div>
+      <h1>Filme Page</h1>
+      <p>Detalhes do filme selecionado.</p>
+    </div>
+  );
+}
+
+export default Filme;
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Bem-vindo ao DevFlix! Explore nossos filmes e séries.</p>
+    </div>
+  );
+}
+
+export default Home;
